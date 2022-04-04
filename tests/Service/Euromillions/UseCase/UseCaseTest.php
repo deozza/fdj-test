dcfbec9aa9f51e26f5fc0a7c62bc54e1ad11b6e9
deozzam <deozza@gmail.com>
2022-04-04 23:40:35 +0200

    written tests for UseCase

--- tests/Service/Euromillions/UseCase/UseCaseTest.php
+++ tests/Service/Euromillions/UseCase/UseCaseTest.php
@@ -2,15 +2,35 @@
 
 namespace App\Tests\Service\Euromillions\UseCase;
 
+use App\Service\Euromillions\Service\Service;
+use App\Service\Euromillions\UseCase\UseCase;
+use App\Service\Euromillions\Model\Result;
+
 use PHPUnit\Framework\TestCase;
+
 
 class UseCaseTest extends TestCase
 {
-    public function getResult_checkServiceIsCalled() {
+    public function test_getResult_checkResultIsReturned() {
+        $mockedReturn = new Result();
+        $mockedReturn->setDate('2022-04-01T21:45:00+02:00');
+        $mockedReturn->setMyMillionId("myMillionId");
+        $mockedReturn->addNumber('number', '01');
 
-    }
+        $mockedService = $this->createMock(Service::class);
+        $mockedService->expects($this->once())
+            ->method('queryResult')
+            ->willReturn($mockedReturn);
+        
+        $useCase = new UseCase();
 
-    public function getResult_checkResultIsReturned() {
+        $reflection = new \ReflectionClass($useCase);
+        $reflection_property = $reflection->getProperty('service');
+        $reflection_property->setAccessible(true);
+        $reflection_property->setValue($useCase, $mockedService);
 
+        $result = $useCase->getResult();
+
+        $this->assertEquals($mockedReturn, $result);
     }
 }
