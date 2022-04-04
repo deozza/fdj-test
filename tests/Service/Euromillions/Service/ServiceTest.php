ddad5fd1eb94526319072d1666d1d85881cc4e1f
deozzam <deozza@gmail.com>
2022-04-04 23:37:05 +0200

    implemented test for service

--- tests/Service/Euromillions/Service/ServiceTest.php
+++ tests/Service/Euromillions/Service/ServiceTest.php
@@ -2,15 +2,35 @@
 
 namespace App\Tests\Service\Euromillions\Service;
 
+use App\Service\Euromillions\Adapter\GuzzleAdapter;
+use App\Service\Euromillions\Service\Service;
+use App\Service\Euromillions\Model\Result;
+
 use PHPUnit\Framework\TestCase;
 
 class ServiceTest extends TestCase
 {
-    public function queryResult_checkCorrectAdapterIsCalled() {
+    public function test_queryResult_checkResultIsReturned() {
 
-    }
+        $mockedReturn = new Result();
+        $mockedReturn->setDate('2022-04-01T21:45:00+02:00');
+        $mockedReturn->setMyMillionId("myMillionId");
+        $mockedReturn->addNumber('number', '01');
 
-    public function queryResult_checkResultIsReturned() {
+        $mockedAdapter = $this->createMock(GuzzleAdapter::class);
+        $mockedAdapter->expects($this->once())
+            ->method('queryResult')
+            ->willReturn($mockedReturn);
+        
+        $service = new Service();
 
+        $reflection = new \ReflectionClass($service);
+        $reflection_property = $reflection->getProperty('adapter');
+        $reflection_property->setAccessible(true);
+        $reflection_property->setValue($service, $mockedAdapter);
+
+        $result = $service->queryResult();
+
+        $this->assertEquals($mockedReturn, $result);
     }
 }
