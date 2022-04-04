397b4deda6756e5b064b746e0d9d624209630c3f
deozzam <deozza@gmail.com>
2022-04-04 23:40:46 +0200

    UseCase is implemented

--- src/Service/Euromillions/UseCase/UseCase.php
+++ src/Service/Euromillions/UseCase/UseCase.php
@@ -3,11 +3,18 @@
 namespace App\Service\Euromillions\UseCase;
 
 use App\Service\Euromillions\Model\Result;
+use App\Service\Euromillions\Service\Service;
 
 class UseCase implements UseCaseInterface{
+    private Service $service;
+
+    public function __construct()
+    {        
+        $this->service = new Service();
+    }
 
     public function getResult(): Result {
-        $result = new Result();
+        $result = $this->service->queryResult();
 
         return $result;
     }
