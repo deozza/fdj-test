5a2307c22634b8e6f8d188256c2e6382a05401c2
deozzam <deozza@gmail.com>
2022-04-04 23:37:16 +0200

    service is implemented

--- src/Service/Euromillions/Service/Service.php
+++ src/Service/Euromillions/Service/Service.php
@@ -2,6 +2,23 @@
 
 namespace App\Service\Euromillions\Service;
 
+use App\Service\Euromillions\Adapter\GuzzleAdapter;
+use App\Service\Euromillions\Adapter\AdapterInterface;
+use App\Service\Euromillions\Model\Result;
+
 class Service implements ServiceInterface{
     
+    private AdapterInterface $adapter;
+
+    public function __construct()
+    {
+        $this->adapter = new GuzzleAdapter();
+    }
+
+    public function queryResult(): Result
+    {
+        $result = $this->adapter->queryResult();
+
+        return $result;
+    }
 }
--- src/Service/Euromillions/Service/ServiceInterface.php
+++ src/Service/Euromillions/Service/ServiceInterface.php
@@ -2,6 +2,8 @@
 
 namespace App\Service\Euromillions\Service;
 
+use App\Service\Euromillions\Model\Result;
+
 interface ServiceInterface {
-    
+    function queryResult(): Result;
 }
